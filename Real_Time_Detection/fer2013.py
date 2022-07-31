92b6354570976e4d596a6debf1d957070bd55810
BhavaniAM <58398927+BhavaniAM@users.noreply.github.com>
2022-07-31 15:34:22 +0530

    Create fer2013.py

---
+++ Real_Time_Detection/fer2013.py
@@ -0,0 +1,38 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import cv2
+
+emotion =  ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+
+model = keras.models.load_model("fer2013.h5")
+font = cv2.FONT_HERSHEY_SIMPLEX
+cam = cv2.VideoCapture(0)
+face_cas = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+while True:
+    ret, frame = cam.read()
+    
+    if ret==True:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cas.detectMultiScale(gray, 1.3,5)
+        
+        for (x, y, w, h) in faces:
+            face_component = gray[y:y+h, x:x+w]
+            fc = cv2.resize(face_component, (48, 48))
+            inp = np.reshape(fc,(1,48,48,1)).astype(np.float32)
+            inp = inp/255.
+            prediction = model.predict(inp)
+            em = emotion[np.argmax(prediction)]
+            score = np.max(prediction)
+            cv2.putText(frame, em+"  "+str(score*100)+'%', (x, y), font, 1, (0, 255, 0), 2)
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
+        cv2.imshow("image", frame)
+        
+        if cv2.waitKey(1) == 27:
+            break
+    else:
+        print ('Error')
+
+cam.release()
+cv2.destroyAllWindows()
